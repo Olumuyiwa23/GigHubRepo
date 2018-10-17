ad47994691b87710e451758fba1dbddfe9f81016
Olumuyiwa <sirolumuyiwa@gmail.com>
2018-10-17 16:15:44 +0100

    Initial Commit

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date {get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime
+        {
+            get { return DateTime.Parse($"{Date} {Time}"); }
+
+        }
+    }
+}
